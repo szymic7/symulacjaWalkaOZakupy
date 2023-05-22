e9f190fc75d88ab54292ba6227417435f3ce75db
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-22 18:55:29 +0200

    Update ChildClient.java

--- Symulacja/src/ChildClient.java
+++ Symulacja/src/ChildClient.java
@@ -1,4 +1,23 @@
-public class ChildClient
+import java.util.Random;
+
+public class ChildClient extends Client
 {
+    private static final double ChanceOfGetting = 50;
+    private static final int speed = 100;
+
+    public ChildClient(int x, int y)
+    {
+        super(x, y);
+    }
+
+    public boolean TryToGet()
+    {
+        Random random = new Random();
+        return random.nextInt() <= ChanceOfGetting;
+    }
 
 }
+
+
+
+
