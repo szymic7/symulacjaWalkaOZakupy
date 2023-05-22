0d7ef24fb9726e9e7a45f4a8d434b1a1962bd615
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-22 18:55:51 +0200

    Update AdultClient.java

--- Symulacja/src/AdultClient.java
+++ Symulacja/src/AdultClient.java
@@ -1,4 +1,23 @@
-public class AdultClient
-{
+import java.util.Random;
 
+public class AdultClient extends Client
+{
+    private static final double ChanceOfGetting = 100;
+    private static final int speed = 75;
+
+    public AdultClient(int x, int y)
+    {
+        super(x, y);
+    }
+
+    public boolean TryToGet()
+    {
+        Random random = new Random();
+        return random.nextDouble() <= ChanceOfGetting;
+    }
 }
+
+
+
+
+
