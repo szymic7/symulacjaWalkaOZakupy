371c0c56eca191563c97892a9df04c499d37bab0
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-22 18:54:32 +0200

    Update ElderlyClient.java

--- Symulacja/src/ElderlyClient.java
+++ Symulacja/src/ElderlyClient.java
@@ -1,4 +1,22 @@
-public class ElderlyClient
-{
+import java.util.Random;
 
+public class ElderlyClient extends Client
+{
+    private static final double ChanceOfGetting = 75;
+    private static final int speed = 50;
+
+    public ElderlyClient(int x, int y)
+    {
+        super(x, y);
+    }
+
+    public boolean TryToGet()
+    {
+        Random random = new Random();
+        return random.nextDouble() <= ChanceOfGetting;
+    }
 }
+
+
+
+
