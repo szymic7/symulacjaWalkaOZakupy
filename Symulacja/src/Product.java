952ae938f831db23640dad314362be3ce4badd03
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-23 15:44:58 +0200

    Add files via upload

---
+++ Symulacja/src/Product.java
@@ -0,0 +1,14 @@
+class Product
+{
+    private boolean isPromotional;
+
+    public Product(boolean isPromotional)
+    {
+        this.isPromotional = isPromotional;
+    }
+
+    public boolean isPromotional()
+    {
+        return isPromotional;
+    }
+}
