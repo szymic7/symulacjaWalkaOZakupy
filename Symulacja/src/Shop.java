91e2e2974047ebc8cd9f02bcb3fce0f50450bb53
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-23 15:50:47 +0200

    Update Shop.java

--- Symulacja/src/Shop.java
+++ Symulacja/src/Shop.java
@@ -1,51 +1,56 @@
+import java.util.Random;
+import java.util.ArrayList;
+
 public class Shop
 {
     private static final int MapSize = 10;
-    private static final int NumberOfProducts = 5;
+    private static final int NumberOfPromotional = 5;
 
     private ArrayList<Client> clients;
     private ArrayList<String> products;
+    private Shelf shelf;
+    private CashRegister cashRegister;
 
     public Shop()
     {
         clients = new ArrayList<>();
-        products = new ArrayList<>();
-
-        for (int i=1; i<=NumberOfProducts; i++)
-        {
-            products.add("Product " + i);
-        }
-
         Random random = new Random();
 
         for(int i=0; i<2; i++)
         {
-            int x = random.nextInt(MapSize);
-            int y = random.nextInt(MapSize);
+            int x = random.nextInt(MapSize+1);
+            int y = random.nextInt(MapSize+1);
 
             clients.add(new ChildClient(x, y));
         }
 
         for(int i=0; i<2; i++)
         {
-            int x = random.nextInt(MapSize);
-            int y = random.nextInt(MapSize);
+            int x = random.nextInt(MapSize+1);
+            int y = random.nextInt(MapSize+1);
 
             clients.add(new AdultClient(x, y));
         }
 
         for(int i=0; i<2; i++)
         {
-            int x = random.nextInt(MapSize);
-            int y = random.nextInt(MapSize);
+            int x = random.nextInt(MapSize+1);
+            int y = random.nextInt(MapSize+1);
 
             clients.add(new ElderlyClient(x, y));
         }
 
+        shelf = new Shelf(NumberOfPromotional);
+        cashRegister = new CashRegister();
     }
 
+    public void RunSimulation()
+    {
+        Random random = new Random();
+
+    }
+
+
 }
-
-
 
 
