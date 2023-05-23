fcc293c10fe606970fd7bd693d8ef4dcc2b9a836
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-23 15:44:34 +0200

    Add files via upload

---
+++ Symulacja/src/Shelf.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+class Shelf
+{
+    private ArrayList<Product> products;
+    private int NumberOfPromotional;
+
+    public Shelf(int NumberOfPromotional)
+    {
+        this.NumberOfPromotional = NumberOfPromotional;
+        products = new ArrayList<>();
+        Random random = new Random();
+
+        for (int i=0; i<10; i++)
+        {
+            boolean isPromotional = false;
+            products.add(new Product(isPromotional));
+        }
+
+        for(int i=0; i<NumberOfPromotional; i++)
+        {
+            int a = random.nextInt(10);
+            boolean isPromotional = true;
+            products.set(a, new Product(isPromotional));
+        }
+    }
+
+    public ArrayList<Product> getProducts()
+    {
+        return products;
+    }
+
+}
