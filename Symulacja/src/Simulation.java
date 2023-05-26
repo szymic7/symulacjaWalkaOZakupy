47582bbeea0ddceb1f5fee33c29d282a79e4ac60
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-27 00:51:25 +0200

    Update Simulation.java

--- Symulacja/src/Simulation.java
+++ Symulacja/src/Simulation.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Simulation
 {
+    private Shop shop;
+    private ArrayList<Client> clients;
+    public Simulation(){
+        this.shop = new Shop();
+        this.clients = new ArrayList<Client>();
+    }
+    public void runSimulation(){
+        Random random = new Random();
+        while(!clients.isEmpty()){
+            // tu bedzie cala iteracja
+        }
+    }
     public static void main(String[] args)
     {
-        Shop shop = new Shop();
-        shop.RunSimulation();
+        // np mozna zrobic tak, ze w argumentach konstruktora obiektu Simulation, beda wymagane argumenty
+        // ktore beda argumentami konstruktora Shop (np. liczba klientow, liczba produktow promocyjnych)
+        Simulation simulation = new Simulation(/* np int numberOfClients, int numberOfPromotionalProducts */);
+        simulation.runSimulation();
     }
 
 }
