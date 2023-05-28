627cc2ea850a5bbd1edd027dcad191a0a9719c17
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-28 15:12:28 +0200

    Update ChildClient.java

--- Symulacja/src/ChildClient.java
+++ Symulacja/src/ChildClient.java
@@ -1,30 +1,11 @@
-import java.util.Random;
-
 public class ChildClient extends Client
 {
-    private static final double ChanceOfGetting = 50;
-    private static final int speed = 100;
+    //private static final double ChanceOfGetting = 50;
+    //private static final int speed = 100;
 
     public ChildClient(int x, int y)
     {
-        super(x, y);
-    }
-
-    public boolean TryToGet(Product product)
-    {
-        if(product.isPromotional()){
-            Random random = new Random();
-            return random.nextInt(100) <= ChanceOfGetting;
-        } else {
-            return false;
-            // zmien kierunek ruchu, chyba ze to zrobimy w klasie Shop, a ta metoda bedzie tylko zwracac boola
-        }
-
-    }
-
-    public void move()
-    {
-
+        super(x, y, 50, 100);
     }
 
 }
