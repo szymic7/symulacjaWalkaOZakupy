53e9b6878f22e6eced91752fbbfc4aa4d9e58134
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-28 15:13:38 +0200

    Update Product.java

--- Symulacja/src/Product.java
+++ Symulacja/src/Product.java
@@ -1,15 +1,24 @@
 class Product
 {
+    private int x;
+    private int y;
     private boolean isPromotional;
 
-    public Product(boolean isPromotional)
+    public Product(int x, int y, boolean isPromotional)
     {
+        this.x = x;
+        this.y = y;
         this.isPromotional = isPromotional;
     }
 
+    public int getX(){
+        return this.x;
+    }
+    public int getY(){
+        return this.y;
+    }
     public boolean isPromotional()
     {
         return isPromotional;
     }
 }
-
