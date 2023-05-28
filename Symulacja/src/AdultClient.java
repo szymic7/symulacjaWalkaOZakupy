636c40e269663a41c677fb07a41da8fda4877646
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-28 15:12:00 +0200

    Update AdultClient.java

--- Symulacja/src/AdultClient.java
+++ Symulacja/src/AdultClient.java
@@ -1,32 +1,11 @@
-import java.util.Random;
-
 public class AdultClient extends Client
 {
-    private static final double ChanceOfGetting = 100;
-    private static final int speed = 75;
+    //private static final double ChanceOfGetting = 100;
+    //private static final int speed = 75;
 
     public AdultClient(int x, int y)
     {
-        super(x, y);
+        super(x, y, 100, 90);
     }
-
-    public boolean TryToGet(Product product)
-    {
-        if(product.isPromotional()) {
-            Random random = new Random();
-            // return random.nextDouble(100) <= ChanceOfGetting;
-            return true;
-            // zawsze bedzie to prawda dla AdultClient, wiec mozemy po prostu zwrocic true
-        } else {
-            return false;
-        }
-    }
-
-    public void move()
-    {
-
-    }
-
+    
 }
-
-
