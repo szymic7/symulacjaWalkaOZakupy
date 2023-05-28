92a25b1a1345e6dfe1035bb4ec9de6b472234f76
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-28 15:13:15 +0200

    Update ElderlyClient.java

--- Symulacja/src/ElderlyClient.java
+++ Symulacja/src/ElderlyClient.java
@@ -1,30 +1,11 @@
-import java.util.Random;
-
 public class ElderlyClient extends Client
 {
-    private static final double ChanceOfGetting = 70;
-    private static final int speed = 50;
+    //private static final double ChanceOfGetting = 70;
+    //private static final int speed = 50;
 
     public ElderlyClient(int x, int y)
     {
-        super(x, y);
-    }
-
-    public boolean TryToGet(Product product)
-    {
-        if(product.isPromotional()){
-            Random random = new Random();
-            return random.nextInt(100) <= ChanceOfGetting;
-        } else {
-            return false;
-            // zmien kierunek ruchu, chyba ze to zrobimy w klasie Shop, a ta metoda bedzie tylko zwracac boola
-        }
-    }
-
-    public void move()
-    {
-
+        super(x, y, 70, 80);
     }
 
 }
-
