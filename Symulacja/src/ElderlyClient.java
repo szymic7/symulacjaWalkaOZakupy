ea4d4605efc4b6e1072cb1603f06e6f444e6f204
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-29 17:49:52 +0200

    Update ElderlyClient.java

--- Symulacja/src/ElderlyClient.java
+++ Symulacja/src/ElderlyClient.java
@@ -1,8 +1,5 @@
 public class ElderlyClient extends Client implements IClient
 {
-    //private static final double ChanceOfGetting = 70;
-    //private static final int speed = 50;
-
     public ElderlyClient(int x, int y)
     {
         super(x, y, 70, 80);
