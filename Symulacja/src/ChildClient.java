3d14fe8be48996e97dcb51a0ef180e0f11358ea7
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-05-29 17:50:00 +0200

    Update ChildClient.java

--- Symulacja/src/ChildClient.java
+++ Symulacja/src/ChildClient.java
@@ -1,8 +1,5 @@
 public class ChildClient extends Client implements IClient
 {
-    //private static final double ChanceOfGetting = 50;
-    //private static final int speed = 100;
-
     public ChildClient(int x, int y)
     {
         super(x, y, 50, 100);
