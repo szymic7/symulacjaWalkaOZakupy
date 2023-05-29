4ffdb5f8d8b0c70c3f65126741c588abb6363066
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-30 00:01:17 +0200

    Update Shop.java

--- Symulacja/src/Shop.java
+++ Symulacja/src/Shop.java
@@ -85,13 +85,11 @@ public class Shop
         return client.getYLocation();
     }
     // musimy ustalic czy location podajemy jako jeden int, czy jako x i y i zmienić najwyzej
-
+    public void clientLeaves(Client client) {
+            if(client.getXLocation()>=18 && client.getYLocation()>=18){
+                clients.remove(client);
+                System.out.println("Klient opuscil sklep.");
+            }
+    }
 
 }
-
-
-
-
-
-
-
