5228e482dc6fced3f9552575257953a0a959bfd7
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-05-30 18:09:34 +0200

    Update Simulation.java

--- Symulacja/src/Simulation.java
+++ Symulacja/src/Simulation.java
@@ -11,14 +11,14 @@ public class Simulation
         Random random = new Random();
         //while(!shop.getClients().isEmpty()){
         for(int i=0; i<50; i++) {
-            for(int j = 0; j < 3; j++) {
+            for(int j = 0; j < shop.getClients().size(); j++) {
                 shop.getClient(j).move();
                 System.out.println("Klient(" + j + "): x = " + shop.getClient(j).getXLocation() + ", y = " + shop.getClient(j).getYLocation());
                 if ((shop.getClient(j).TryToGet(shop.getProducts())) > -1) {
                     if (shop.getClient(j).TryToBuy(shop.getProducts().get(shop.getClient(j).TryToGet(shop.getProducts())))) {
-                        System.out.println("Klient(" + j + ") zakupil produkt promocyjny.");
+                        System.out.println("Klient(" + j + ") zakupil produkt.");
                     } else {
-                        System.out.println("Klientowi(" + j + ") nie udalo sie zakupic produktu promocyjnego.");
+                        System.out.println("Klientowi(" + j + ") nie udalo sie zakupic produktu.");
                     }
                 }
                 shop.clientLeaves(shop.getClient(j));
@@ -27,8 +27,6 @@ public class Simulation
     }
     public static void main(String[] args)
     {
-        // np mozna zrobic tak, ze w argumentach konstruktora obiektu Simulation, beda wymagane argumenty
-        // ktore beda argumentami konstruktora Shop (np. liczba klientow, liczba produktow promocyjnych)
         Simulation simulation = new Simulation(60);
         simulation.runSimulation();
     }
