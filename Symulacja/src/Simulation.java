719f644db3a5b993cee44c764f906680085d002f
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-06-04 22:08:57 +0200

    Update Simulation.java

--- Symulacja/src/Simulation.java
+++ Symulacja/src/Simulation.java
@@ -5,6 +5,9 @@ public class Simulation
 {
     private Shop shop;
     private HashMap<Client, Integer> clientsIndexes;
+    private static int boughtByChild = 0;
+    private static int boughtByAdult = 0;
+    private static int boughtByElderly = 0;
 
     public Simulation(int numberOfPromotional, int numberOfChild, int numberOfAdult, int numberOfElderly)
     {
@@ -32,78 +35,97 @@ public class Simulation
         {
             for (int i=0; i<shop.getClients().size(); i++)
             {
+
                 // interakcje miedzy klientami
+                int interactions=0;
                 for(int j=0; j<shop.getClients().size(); j++)
                 {
                     if((shop.getClient(i).getXLocation() == shop.getClient(j).getXLocation()) &&
                             (shop.getClient(i).getYLocation() == shop.getClient(j).getYLocation()))
+                    // pierwszy warunek - dwoch klientow na tym samym polu
                     {
                         if((clientsIndexes.get(shop.getClient(i))<numberOfChild && clientsIndexes.get(shop.getClient(j))>=numberOfChild) ||
                                 (clientsIndexes.get(shop.getClient(i))<(numberOfAdult+numberOfChild) && clientsIndexes.get(shop.getClient(j))>=(numberOfAdult+numberOfChild)))
+                        // drugi warunek - (client i to ChildClient a client j to AdultClient albo ElderlyClient) lub
+                            // (klient i to ChildClient albo AdultClient a client j to ElderlyClient)
                         {
                             System.out.println("Klient(" + clientsIndexes.get(shop.getClient(i)) + ") przepuscil " +
                                     "klienta(" + clientsIndexes.get(shop.getClient(j)) + ").");
-                            i++;
+                            interactions++;
                         }
                     }
                 }
 
-                // ruch klientów
-                shop.getClient(i).move();
-                System.out.println("Klient(" + clientsIndexes.get(shop.getClient(i)) + ") x = " + shop.getClient(i).getXLocation() + ", y = " + shop.getClient(i).getYLocation());
-
-                if((shop.getClient(i).TryToGet(shop.getProducts())) > -1)
+                if(interactions==0)
                 {
-                    if(shop.getClient(i).TryToBuy(shop.getProducts().get(shop.getClient(i).TryToGet(shop.getProducts()))))
+                    shop.getClient(i).move(); // ruch klienta
+                    System.out.println("Klient(" + clientsIndexes.get(shop.getClient(i)) + ") x = " + shop.getClient(i).getXLocation() + ", y = " + shop.getClient(i).getYLocation());
+
+                    // sprawdzenie, czy stojac na danym polu klient moze wejsc w interakcje z produktem promocyjnym
+                    if ((shop.getClient(i).tryToGet(shop.getProducts())) > -1)
                     {
-                        System.out.println("Klient(" + clientsIndexes.get(shop.getClient(i)) + ") zakupil produkt promocyjny.");
+                        // sprawdzenie, czy klient jest w stanie "dosiegnac"/"dostrzec" produkt i podniesc go
+                        if (shop.getClient(i).tryToBuy(shop.getProducts().get(shop.getClient(i).tryToGet(shop.getProducts()))))
+                        {
+                            System.out.println("Klient(" + clientsIndexes.get(shop.getClient(i)) + ") zakupil produkt promocyjny.");
+                            if (clientsIndexes.get(shop.getClient(i)) < numberOfChild)
+                            {
+                                boughtByChild++;
+                            }
+                            else if (clientsIndexes.get(shop.getClient(i)) >= numberOfChild && clientsIndexes.get(shop.getClient(i)) < (numberOfChild + numberOfAdult))
+                            {
+                                boughtByAdult++;
+                            }
+                            else if (clientsIndexes.get(shop.getClient(i)) >= (numberOfChild + numberOfAdult))
+                            {
+                                boughtByElderly++;
+                            }
+                        }
+                        else
+                        {
+                            System.out.println("Klientowi(" + clientsIndexes.get(shop.getClient(i)) + ") nie udalo sie zakupic produktu promocyjnego.");
+                        }
                     }
-                    else
-                    {
-                        System.out.println("Klientowi(" + clientsIndexes.get(shop.getClient(i)) + ")nie udalo sie zakupic produktu promocyjnego.");
-                    }
+
+                    // sprawdzenie, czy klient trafil do kasy
+                    shop.clientLeaves(shop.getClient(i));
                 }
-                shop.clientLeaves(shop.getClient(i));
             }
         }
 
-        //zliczanie rezultatów symulacji
-        System.out.println();
-        System.out.println("\nPODSUMOWANIE SYMULACJI\n");
-        System.out.println("Ilość agentów typu Child: " + numberOfChild);
-        System.out.println("Ilość agentów typu Adult: " + numberOfAdult);
-        System.out.println("Ilość agentów typu Elderly: " + numberOfElderly);
-        System.out.println("Ilość wszystkich produktów promocyjnych: " + numberOfPromotional);
+        // zliczanie rezultatow symulacji
+        System.out.println("\n\nWYNIKI SYMULACJI\n");
+        System.out.println("Liczba klientow typu Child: " + numberOfChild);
+        System.out.println("Liczba klientow typu Adult: " + numberOfAdult);
+        System.out.println("Liczba klientow typu Elderly: " + numberOfElderly);
+        System.out.println("Liczba produktow promocyjnych na poczatku symulacji: " + numberOfPromotional);
 
-        int ArePromotionalSold = 0;
+        // zliczenie, ile zostalo promocyjnych produktow
+        int promotionalLeft = 0;
         for(Product product : shop.getProducts())
         {
-            if(product.isPromotional()==false)
+            if(product.isPromotional())
             {
-                ArePromotionalSold++;
+                promotionalLeft++;
             }
         }
-        if(ArePromotionalSold==60)
-        {
-            System.out.println("Sprzedano wszystkie produkty promocyjne!!!");
-        }
-        else
-        {
-            System.out.println("Ilość niesprzedanych produktów promocyjnych: " + (60-ArePromotionalSold));
-        }
+
+        System.out.println("Liczba sprzedanych produktow promocyjnych: " + (numberOfPromotional-promotionalLeft));
+        System.out.println("Produkty kupione przez klientow typu Child: " + boughtByChild);
+        System.out.println("Produkty kupione przez klientow typu Adult: " + boughtByAdult);
+        System.out.println("Produkty kupione przez klientow typu Elderly: " + boughtByElderly);
 
     }
 
     public static void main(String[] args)
     {
-        int numberOfPromotional = 60;
+        int numberOfPromotional = 40;
         int numberOfChild = 2;
-        int numberOfAdult = 3;
-        int numberOfElderly = 1;
+        int numberOfAdult = 1;
+        int numberOfElderly = 3;
 
         Simulation simulation = new Simulation(numberOfPromotional, numberOfChild, numberOfAdult, numberOfElderly);
         simulation.runSimulation(numberOfPromotional, numberOfChild, numberOfAdult, numberOfElderly);
     }
 
 }
-
