8410005488f0a52c46ede19f11ac24f447e7e0ed
lipa13 <134206524+lipa13@users.noreply.github.com>
2023-06-08 17:08:58 +0200

    Update SwingWindow.java

--- Symulacja/src/SwingWindow.java
+++ Symulacja/src/SwingWindow.java
@@ -4,51 +4,93 @@ import javax.swing.event.*;
 
 public class SwingWindow
 {
+    private int numberOfPromotional;
+    private int numberOfChild;
+    private int numberOfAdult;
+    private int numberOfElderly;
+
     private JFrame frame;
+    private JSlider promotionalSlider;
     private JSlider childSlider;
     private JSlider adultSlider;
     private JSlider elderlySlider;
+    private JLabel promotionalLabel;
     private JLabel childLabel;
     private JLabel adultLabel;
     private JLabel elderlyLabel;
     private JPanel storePanel;
 
-    public SwingWindow()
+    public SwingWindow(int numberOfPromotional, int numberOfChild, int numberOfAdult, int numberOfElderly)
     {
+        this.numberOfPromotional = numberOfPromotional;
+        this.numberOfChild = numberOfChild;
+        this.numberOfAdult = numberOfAdult;
+        this.numberOfElderly = numberOfElderly;
+
         initialize();
     }
 
     private void initialize()
     {
+        //ustawienia okna
         frame = new JFrame();
         frame.setBounds(200, 200, 850, 550);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().setLayout(null);
 
+        //ustawienia mapy
         storePanel = new JPanel();
-        storePanel.setBounds(45,80,480,360);
+        storePanel.setBounds(60,70,400,400);
         storePanel.setBackground(Color.black);
 
         frame.add(storePanel);
 
+        //nagłówek
         JLabel titleLabel = new JLabel("Symulacja zakupów");
         titleLabel.setFont(new Font("Tahoma", Font.BOLD, 16));
         titleLabel.setBounds(330, 10, 200, 20);
         frame.getContentPane().add(titleLabel);
 
+        //promotional slider
+        JLabel promotionalTitleLabel = new JLabel("Promotional:");
+        promotionalTitleLabel.setBounds(500, 100, 80, 20);
+        frame.getContentPane().add(promotionalTitleLabel);
+
+        promotionalLabel = new JLabel(String.valueOf(numberOfPromotional));
+        promotionalLabel.setBounds(590, 100, 20, 20);
+        frame.getContentPane().add(promotionalLabel);
+
+        promotionalSlider = new JSlider(JSlider.HORIZONTAL, 0,60, numberOfPromotional);
+        promotionalSlider.setMinorTickSpacing(2);
+        promotionalSlider.setMajorTickSpacing(20);
+        promotionalSlider.setPaintTicks(true);
+        promotionalSlider.setPaintLabels(true);
+        promotionalSlider.setBounds(610, 100, 190, 50);
+        promotionalSlider.addChangeListener(new ChangeListener() {
+            public void stateChanged(ChangeEvent e)
+            {
+                int value = promotionalSlider.getValue();
+                promotionalLabel.setText(String.valueOf(value));
+            }
+        });
+
+        frame.getContentPane().add(promotionalSlider);
+
         //child slider
         JLabel childTitleLabel = new JLabel("ChildClient:");
-        childTitleLabel.setBounds(570, 185, 80, 20);
+        childTitleLabel.setBounds(510, 160, 80, 20);
         frame.getContentPane().add(childTitleLabel);
 
-        childLabel = new JLabel("0");
-        childLabel.setBounds(650, 185, 20, 20);
+        childLabel = new JLabel(String.valueOf(numberOfChild));
+        childLabel.setBounds(590, 160, 20, 20);
         frame.getContentPane().add(childLabel);
 
-        childSlider = new JSlider();
-        childSlider.setMinimum(0);
-        childSlider.setMaximum(10);
-        childSlider.setBounds(670, 185, 150, 20);
+        childSlider = new JSlider(JSlider.HORIZONTAL, 0,6, numberOfChild);
+        childSlider.setMinorTickSpacing(1);
+        childSlider.setMajorTickSpacing(2);
+        childSlider.setPaintTicks(true);
+        childSlider.setPaintLabels(true);
+        childSlider.setBounds(610, 160, 180, 50);
         childSlider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e)
             {
@@ -61,17 +103,19 @@ public class SwingWindow
 
         //adult slider
         JLabel adultTitleLabel = new JLabel("AdultClient:");
-        adultTitleLabel.setBounds(570, 225, 80, 20);
+        adultTitleLabel.setBounds(510, 215, 80, 20);
         frame.getContentPane().add(adultTitleLabel);
 
-        adultLabel = new JLabel("0");
-        adultLabel.setBounds(650, 225, 20, 20);
+        adultLabel = new JLabel(String.valueOf(numberOfAdult));
+        adultLabel.setBounds(590, 215, 20, 20);
         frame.getContentPane().add(adultLabel);
 
-        adultSlider = new JSlider();
-        adultSlider.setMinimum(0);
-        adultSlider.setMaximum(10);
-        adultSlider.setBounds(670, 225, 150, 20);
+        adultSlider = new JSlider(JSlider.HORIZONTAL, 0,6, numberOfAdult);
+        adultSlider.setMinorTickSpacing(1);
+        adultSlider.setMajorTickSpacing(2);
+        adultSlider.setPaintTicks(true);
+        adultSlider.setPaintLabels(true);
+        adultSlider.setBounds(610, 215, 180, 50);
         adultSlider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e)
             {
@@ -84,17 +128,19 @@ public class SwingWindow
 
         //elderly slider
         JLabel elderlyTitleLabel = new JLabel("ElderlyClient:");
-        elderlyTitleLabel.setBounds(570, 265, 80, 20);
+        elderlyTitleLabel.setBounds(510, 265, 80, 20);
         frame.getContentPane().add(elderlyTitleLabel);
 
-        elderlyLabel = new JLabel("0");
-        elderlyLabel.setBounds(650, 265, 20, 20);
+        elderlyLabel = new JLabel(String.valueOf(numberOfElderly));
+        elderlyLabel.setBounds(590, 265, 20, 20);
         frame.getContentPane().add(elderlyLabel);
 
-        elderlySlider = new JSlider();
-        elderlySlider.setMinimum(0);
-        elderlySlider.setMaximum(10);
-        elderlySlider.setBounds(670, 265, 150, 20);
+        elderlySlider = new JSlider(JSlider.HORIZONTAL, 0,6, numberOfElderly);
+        elderlySlider.setMinorTickSpacing(1);
+        elderlySlider.setMajorTickSpacing(2);
+        elderlySlider.setPaintTicks(true);
+        elderlySlider.setPaintLabels(true);
+        elderlySlider.setBounds(610, 265, 180, 50);
         elderlySlider.addChangeListener(new ChangeListener() {
             public void stateChanged(ChangeEvent e)
             {
