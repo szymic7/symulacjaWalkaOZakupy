e8263a597f9741c8a53a833c9d23f820f18bd9a9
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-09-24 14:37:53 +0200

    Update ChildClient.java

--- Symulacja/src/ChildClient.java
+++ Symulacja/src/ChildClient.java
@@ -1,5 +1,13 @@
+/**
+ * Klasa reprezentująca klienta dziecko, dziedzicząca po klasie <code>Client</code>
+ */
 public class ChildClient extends Client
 {
+    /**
+     * Konstruktor obiektu typu <code>ChildClient</code>, wywołujący konstruktor klasy <code>Client</code>
+     * @param x współrzędna x klienta
+     * @param y współrzędna y klienta
+     */
     public ChildClient(int x, int y)
     {
         super(x, y, 50, 100);
