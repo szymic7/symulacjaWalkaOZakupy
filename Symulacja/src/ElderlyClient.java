a65be2eec6b42f1bb8381c97dbdcf2fa03e58feb
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-09-24 14:37:15 +0200

    Update ElderlyClient.java

--- Symulacja/src/ElderlyClient.java
+++ Symulacja/src/ElderlyClient.java
@@ -1,5 +1,13 @@
+/**
+ * Klasa reprezentująca klienta seniora, dziedzicząca po klasie <code>Client</code>
+ */
 public class ElderlyClient extends Client
 {
+    /**
+     * Konstruktor obiektu typu <code>ElderlyClient</code>, wywołujący konstruktor klasy <code>Client</code>
+     * @param x współrzędna x klienta
+     * @param y współrzędna y klienta
+     */
     public ElderlyClient(int x, int y)
     {
         super(x, y, 70, 70);
