95d7bcc88ff1ea34696048ae03bd5f31ed46b7f2
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-09-24 14:36:53 +0200

    Update AdultClient.java

--- Symulacja/src/AdultClient.java
+++ Symulacja/src/AdultClient.java
@@ -1,5 +1,13 @@
+/**
+ * Klasa reprezentująca dorosłego klienta, dziedzicząca po klasie <code>Client</code>
+ */
 public class AdultClient extends Client
 {
+    /**
+     * Konstruktor obiektu typu <code>AdultClient</code>, wywołujący konstruktor klasy <code>Client</code>
+     * @param x współrzędna x klienta
+     * @param y współrzędna y klienta
+     */
     public AdultClient(int x, int y)
     {
         super(x, y, 100, 85);
