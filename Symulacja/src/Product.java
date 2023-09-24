5af4d124ec865f410ef4d9d5f29a6eba641ce494
szymic7 <134203800+szymic7@users.noreply.github.com>
2023-09-24 14:35:53 +0200

    Update Product.java

--- Symulacja/src/Product.java
+++ Symulacja/src/Product.java
@@ -1,10 +1,19 @@
-class Product
+/**
+ * Klasa reprezentująca produkt
+ */
+public class Product
 {
     private int x;
     private int y;
     private boolean promotional;
     private boolean sold;
 
+    /**
+     * Konstruktor obiektu <code>Product</code>
+     * @param x wspolrzedna x produktu (a dokładniej współrzędna x pola, z którego można wejść w interakcję z porduktem)
+     * @param y wspolrzedna y produktu
+     * @param promotional czy dany produkt jest produktem promocyjnym?
+     */
     public Product(int x, int y, boolean promotional)
     {
         this.x = x;
@@ -13,19 +22,42 @@ class Product
         this.sold = false;
     }
 
+    /**
+     * Metoda zwracająca współrzędną x produktu
+     * @return Współrzędna x produktu
+     */
     public int getX(){
         return this.x;
     }
+
+    /**
+     * Metoda zwracająca współrzędną y produktu
+     * @return Współrzędna y produktu
+     */
     public int getY(){
         return this.y;
     }
+
+    /**
+     * Metoda zwraca zmienną typu <code>boolean</code> informującą, czy dany produkt jest produktem promocyjnym
+     * @return Zmienna <code>promotional</code> mówiąca, czy dany produkt jest produktem promocyjnym
+     */
     public boolean getPromotional()
     {
         return this.promotional;
     }
+
+    /**
+     * Metoda zwraca zmienną typu <code>boolean</code> informującą, czy dany produkt został sprzedany
+     * @return Zmienna <code>sold</code> mówiąca, czy dany produkt jest sprzedany
+     */
     public boolean getSold(){
         return this.sold;
     }
+
+    /**
+     * Metoda mienia wartość zmiennej <code>sold</code> produktu na <code>true</code> - oznacza produkt jako sprzedany
+     */
     public void isSold(){
         this.sold = true;
     }
